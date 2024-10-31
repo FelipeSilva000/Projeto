4ab18ad11e5b38ef3c17d98481059ed1b682cc39
Felipe Silva <fellipecllash@gmail.com>
2024-10-31 18:03:45 -0300

    comentando js

--- Projeto/animate.js
+++ Projeto/animate.js
@@ -1,30 +1,34 @@
-$(document).ready(function (){
-    $('.btn-comprar').on('click',function(){  //botao comprar
-        $('.container').toggleClass('active') //inserir dados
+//Uso de J QUERY
+
+$(document).ready(function (){ //carregar todo documento
+    $('.btn-comprar').on('click',function(){  //ouvinte de elemento botao comprar,
+        $('.container').toggleClass('active') //quando clicado add classe active em .container 
         
-        document.querySelector('.container').scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'})
+        document.querySelector('.container').scrollIntoView({ //usa a classe para localizar o elemento
+            behavior: 'smooth',  //rolagem suave
+            block: 'start'})     //rolagem ao topo do bloco
 
     });
 });
 
 
 $(document).ready(function (){
-    $('.btn-animate-btn').on('click',function(){  //botao ver produtos
-        $('.produtos').toggleClass('active') //inserir dados
+    $('.btn-animate-btn').on('click',function(){ //ouvinte de elemento botao ver produtos
+        $('.produtos').toggleClass('active') //quando clicado add classe active em .produtos
 
-        document.querySelector('.produtos2').scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'})
+        document.querySelector('.produtos2').scrollIntoView({ //usa a classe para localizar o elemento
+            behavior: 'smooth',  //rolagem suave
+            block: 'start'})     //rolagem ao topo do bloco
 
     });
 });
 
 
-const botao = document.getElementById('pedido');
-const mensagem = document.getElementById('mensagem');
+const botao = document.getElementById('pedido');  //busca pelo id
+const mensagem = document.getElementById('mensagem');  //busca pelo id
 
-botao.addEventListener('click', () => {
-    mensagem.style.display = 'block'; // Exibe a mensagem
+botao.addEventListener('click', () => { //ouvinte de evento
+    mensagem.style.display = 'block'; // muda o display none para block e exibe a mensagem
 });
+
+

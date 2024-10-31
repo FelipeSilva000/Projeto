d43f615a8a3f2d45a52c8834478f3ee8e63843de
Felipe Silva <fellipecllash@gmail.com>
2024-10-31 17:40:13 -0300

    animation

--- Projeto/animate.js
+++ Projeto/animate.js
@@ -1,14 +1,23 @@
 $(document).ready(function (){
     $('.btn-comprar').on('click',function(){  //botao comprar
         $('.container').toggleClass('active') //inserir dados
+        
+        document.querySelector('.container').scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'})
+
     });
 });
-
 
 
 $(document).ready(function (){
     $('.btn-animate-btn').on('click',function(){  //botao ver produtos
         $('.produtos').toggleClass('active') //inserir dados
+
+        document.querySelector('.produtos2').scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'})
+
     });
 });
 
